a971c408027bfa0f7548a8b1ed9f1ea31551d77b
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-17 23:12:52 +0100

    changed the strings to numbers

--- calc.js
+++ calc.js
@@ -1,4 +1,8 @@
-const num1 = window.prompt('Please add a number', '1,2,3..');
-const num2 = window.prompt('Please add another number','1,2,3..');
+let num1 = window.prompt('Please add a number', '1,2,3..');
+let num2 = window.prompt('Please add another number','1,2,3..');
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
 const result = num1 + num2;
+
+
 let sum = document.querySelector('#sum').innerHTML = result;
