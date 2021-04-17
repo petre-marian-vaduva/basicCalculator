10b23d95daebc7dddae0a54e5c1f773dcf70637f
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-17 23:01:44 +0100

    add DOM

---
+++ calc.js
@@ -0,0 +1,4 @@
+const num1 = window.prompt('Please add a number', '1,2,3..');
+const num2 = window.prompt('Please add another number','1,2,3..');
+const result = num1 + num2;
+let sum = document.querySelector('#sum').innerHTML = result;
